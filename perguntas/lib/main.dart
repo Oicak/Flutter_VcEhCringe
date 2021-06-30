f9dd4347a1d4e4dbdd2d1e421244ba109d8a60df
Kácio <63382277+Oicak@users.noreply.github.com>
2021-06-30 14:52:25 -0300

    Commit - Aula 41

--- perguntas/lib/main.dart
+++ perguntas/lib/main.dart
@@ -4,15 +4,53 @@ main() {
   runApp(new PerguntaApp());
 }
 
-class PerguntaApp extends StatelessWidget {
+class PerguntaAppState extends State<PerguntaApp> {
+  int perguntaSelecionada = 0;
+
+  void resposta() {
+    setState(() {
+      perguntaSelecionada++;
+    });
+    print(perguntaSelecionada);
+  }
+
+  final perguntas = [
+    "Qual é o seu sexo?",
+    "Em que ano você nasceu?",
+    "Você usa calça Skin?",
+    "Você fala de Boletor"
+  ];
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Olá Flutter!'),
+        body: Column(
+          children: [
+            Text(perguntas[perguntaSelecionada]),
+            TextButton(
+              child: Text('Resposta 1'),
+              onPressed: resposta,
+            ),
+            TextButton(
+              child: Text('Resposta 2'),
+              onPressed: resposta,
+            ),
+            TextButton(
+              child: Text('Resposta 3'),
+              onPressed: resposta,
+            )
+          ],
+        ),
       ),
     );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  PerguntaAppState createState() {
+    return PerguntaAppState();
   }
 }
